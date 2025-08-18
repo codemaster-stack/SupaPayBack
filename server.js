936f31543aab3c24a95c2b4c55a2376b82beddd5
SupaPay <supaapay@gmail.com>
2025-08-18 08:59:04 +0100

    crrt

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(helmet({
 app.use(cors({
   origin: process.env.NODE_ENV === 'production' 
     ? [
-        'https://supapay-jade.vercel.app', // Replace with your actual frontend URL
+        'https://supapay.netlify.app', // Replace with your actual frontend URL
         
       ] 
     : [
